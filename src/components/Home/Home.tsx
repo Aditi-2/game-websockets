e23ab34137f9f4f0ada35d862247c3082b385c2d
Aditi Gupta <aditig734@gmail.com>
2022-03-19 15:06:34 +0100

    fix: naming

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Header } from '../Header/Header';
 import { Wrapper, GameWrapper } from './Home.styled';
-import { Sidebar } from '../sidebar/Sidebar';
+import { Sidebar } from '../Sidebar/Sidebar';
 import { GameRoom } from '../GameRoom/GameRoom';
 
 export const Home = () => (
---
+++ src/components/Sidebar/MenuItem/MenuItem.styled.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const MenuWrapper = styled.div`
+  cursor: pointer;
+  background-color: #ffffff;
+  padding: 2rem;
+`;
+
+export const ItemContent = styled.p`
+  margin: 0;
+  color: #205a6d;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+`;
